2440a1ce05cc9cca9343476eca5db2520258be01
dah4oz <dah4oz@hotmail.com>
2016-08-06 00:13:45 +0300

    Automatic request added

--- SimpleForexApp/app/src/main/java/com/ramram/android/simpleforexapp/MainActivity.java
+++ SimpleForexApp/app/src/main/java/com/ramram/android/simpleforexapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ramram.android.simpleforexapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.android.volley.Response;
@@ -18,13 +19,20 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private static final String REQUEST_URL = "http://eu.tradenetworks.com/QuotesBox/quotes/GetQuotesBySymbols";
+    private static final int REQUEST_DELAY = 500;
+
     private List<ForexQuote> mQuotesList = new ArrayList<>();
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestData();
 
+    }
+
+    private void requestData(){
         Map<String, String> headers = new HashMap<>();
         Map<String, String> params = new HashMap<>();
         CustomRequest<QuotesResponse> quoteRequest = new CustomRequest<>(REQUEST_URL, QuotesResponse.class, headers, params,
@@ -32,17 +40,26 @@ public class MainActivity extends AppCompatActivity {
         NetworkManager.getInstance(this).addToRequestQue(quoteRequest);
     }
 
+
     Response.Listener<QuotesResponse> responseListener = new Response.Listener<QuotesResponse>() {
         @Override
         public void onResponse(QuotesResponse quotesResponse) {
             mQuotesList = quotesResponse.getQuoteList();
+            mHandler.postDelayed(RequestScheduler, REQUEST_DELAY);
         }
     };
 
     Response.ErrorListener errorListener = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError volleyError) {
+            //TODO: show an error message...
+        }
+    };
 
+    Runnable RequestScheduler = new Runnable() {
+        @Override
+        public void run() {
+            requestData();
         }
     };
 }
